c909cf257ea4e80794a470e944600ae161810d70
Eduardo Turiño <eturino@gmail.com>
2012-08-08 12:03:54 +0200

    toArray()

--- ToArrayAbleFull.php
+++ ToArrayAbleFull.php
@@ -4,7 +4,7 @@
  */
 interface EtuDev_Interfaces_ToArrayAbleFull extends EtuDev_Interfaces_ToArrayAble {
 
-	public function toArray($level = null, $toArrayPseudoArraysAndRows = false);
+	public function toArray($level = null, $toArrayToArrayables = false);
 
 	public function toArrayFull();
 }
